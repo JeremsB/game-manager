2fa43a9c5322fcf1debb87125d33ace7587fba4e
Jérémy <jerem.blcn@gmail.com>
2019-12-11 16:53:58 +0100

    Exercice 3

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameListComponent implements OnInit {
 
-  gamePicture = 'https://image.shutterstock.com/image-photo/cheerful-businessman-thumbs-posing-smiling-260nw-371076662.jpg';
+  cards = [
+    ['Un super jeu', 'Gestion', 'https://image.shutterstock.com/image-photo/cheerful-businessman-thumbs-posing-smiling-260nw-371076662.jpg', 9, 'Vu le titre ce jeu est probablement super'],
+    ['Un super jeu', 'RPG', 'https://image.shutterstock.com/image-photo/image-successful-old-woman-showing-600w-122204170.jpg', 8, 'Vu le titre ce jeu est probablement super'],
+    ['Un super jeu', 'FPS', 'https://image.shutterstock.com/image-photo/young-african-american-man-gives-260nw-157500044.jpg', 9, 'Vu le titre ce jeu est probablement super'],
+    ['Un super jeu', 'Gestion', 'https://image.shutterstock.com/image-photo/happy-senior-man-giving-thumb-600w-73143208.jpg', 10, 'Vu le titre ce jeu est probablement super'],
+    ['Un super jeu', 'FPS', 'https://image.shutterstock.com/image-photo/businessman-showing-thumbs-closeup-shot-600w-533368390.jpg', 9, 'Vu le titre ce jeu est probablement super']
+  ];
   constructor() { }
 
   ngOnInit() {
